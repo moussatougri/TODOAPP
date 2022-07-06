96780ee53c0c7aa03cd20e96192969f12512a036
unknown <tougrimoussa3@gmail.com>
2022-07-06 21:45:32 +0200

    refactoring: put all funtion in a constructor

--- script.js
+++ script.js
@@ -1,51 +1,63 @@
 const ul = document.querySelector(".todo-list");
 const input = document.querySelector(".todos-input");
+const allDoneTodosInp = document.querySelector(".done-todos")
 const arrayOfAllTodos = [];
-const liElement = document.createElement("li");
+const arrayOfDoneTodos = [];
 
 
 
 
 
 
-function listOfAllTodos() {
-  const todos = input.value;
-  const liText = document.createTextNode(todos);
-  console.log(liElement);
-  liElement.appendChild(liText);
-  ul.appendChild(liElement);
-  arrayOfAllTodos.push(todos)
-  input.value = "";
-};
 
-function checkboxInLiElement() {
-  const checkbox = document.createElement("input");
-  checkbox.setAttribute("type", "checkbox");
-  liElement.appendChild(checkbox)
-  
-  
-  checkbox.addEventListener("click", function (e) {
-    if (e.target.checked) {
-      liElement.classList.add("checked")
-    }
-  
-  })
-};
+class Todo {
+  constructor(todo) {
+    this.todos = todo;
+    this.checked = false;
+    this.createTodo()
+  }
+  createTodo() {
+    //create li element
+    const liElement = document.createElement("li");
+    const liText = document.createTextNode(this.todos);
+    liElement.appendChild(liText);
+    ul.appendChild(liElement);
+    arrayOfAllTodos.push(liText)
+    input.value = "";
+
+    //checkbox in li element
+    const checkbox = document.createElement("input");
+    checkbox.setAttribute("type", "checkbox");
+    liElement.appendChild(checkbox)
 
 
+    checkbox.addEventListener("click", function (e) {
+      if (e.target.checked) {
+        this.checked = this.checked !== false;
+      }
+      else {
+        this.checked = this.checked !== false;
+      }
+    })
+  }
+}
+
+//class in an function
+function newTodo() {
+  new Todo(input.value);
+}
 
 
 
+
+//all EventListener in an function
 function eventListener() {
+  //press enter EventListener
   input.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
-      listOfAllTodos();
-      checkboxInLiElement();
+      newTodo() 
     }
   });
-};
+}
 
-eventListener();
-
-
-
+eventListener()
