7a1dffe8f4513610cc542e733de748534c189d5f
unknown <tougrimoussa3@gmail.com>
2022-07-08 17:43:57 +0200

    filter function added

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const ul = document.querySelector(".todo-list");
+const todoList = document.querySelector(".todo-list");
 const input = document.querySelector(".todos-input");
 const allDoneTodosInp = document.querySelector(".done-todos");
+const sectionOfFilterTodo = document.querySelector(".filter-todos");
 let arrayOfAllTodos = [];
-let arrayOfDoneTodos = [];
 
 function createTodo() {
   //create li element
@@ -12,6 +12,8 @@ function createTodo() {
 
   // delete Input field
   input.value = "";
+
+  newItem.classList.add("open");
 
   //checkbox in li element
   const checkbox = document.createElement("input");
@@ -27,6 +29,7 @@ function createTodo() {
   checkbox.addEventListener("change", function (e) {
     if (e.target.checked === true) {
       newItem.setAttribute("checked", "true");
+      newItem.classList.remove("open");
       newItem.classList.add("checked");
     } else {
       newItem.removeAttribute("checked");
@@ -41,8 +44,38 @@ function createTodo() {
 
   //appendChild
   newItem.appendChild(checkbox);
-  ul.appendChild(newItem);
+  todoList.appendChild(newItem);
 }
+
+function filterAllDoneTodos(e) {
+  const todos = todoList.childNodes;
+  todos.forEach(function (todo) {
+    switch (e.target.value) {
+      //filter all todos
+      case "all":
+        todo.style.display = "list-item";
+        break;
+      //filter all complete todos
+      case "done":
+        if (todo.classList.contains("checked")) {
+          todo.style.display = "list-item";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+      //filter all uncomplete todos
+      case "open":
+        if (todo.classList.contains("open")) {
+          todo.style.display = "list-item";
+        } else {
+          todo.style.display = "none";
+        }
+        break;
+    }
+  });
+}
+
+console.log(arrayOfAllTodos);
 
 //all EventListener in an function
 function eventListener() {
@@ -52,6 +85,8 @@ function eventListener() {
       createTodo();
     }
   });
+
+  sectionOfFilterTodo.addEventListener("click", filterAllDoneTodos);
 }
 
 eventListener();
