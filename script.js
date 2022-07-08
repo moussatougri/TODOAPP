2fadf0842354bf0d2f277e18c93e84840a2526a1
unknown <tougrimoussa3@gmail.com>
2022-07-08 17:12:02 +0200

    new script/change all function

--- script.js
+++ script.js
@@ -1,63 +1,55 @@
 const ul = document.querySelector(".todo-list");
 const input = document.querySelector(".todos-input");
-const allDoneTodosInp = document.querySelector(".done-todos")
-const arrayOfAllTodos = [];
-const arrayOfDoneTodos = [];
+const allDoneTodosInp = document.querySelector(".done-todos");
+let arrayOfAllTodos = [];
+let arrayOfDoneTodos = [];
 
+function createTodo() {
+  //create li element
+  let newItem = document.createElement("li");
+  //put text on the new Item
+  newItem.innerText = input.value;
 
+  // delete Input field
+  input.value = "";
 
+  //checkbox in li element
+  const checkbox = document.createElement("input");
+  checkbox.setAttribute("type", "checkbox");
 
+  //set Attribute checked
+  newItem.setAttribute("checked", "false");
 
+  arrayOfAllTodos.push(newItem);
+  //
+  checkbox.addEventListener("change", function (e) {
+    if (e.target.checked === true) {
+      newItem.setAttribute("checked", "true");
+      newItem.classList.add("checked");
+    } else {
+      newItem.removeAttribute("checked");
+      newItem.classList.remove("checked");
+      newItem.setAttribute("checked", "false");
+    }
+    //update array of all Todos
+    arrayOfAllTodos.forEach((element, index) => {
+      arrayOfAllTodos[index] = element;
+    });
+  });
 
-
-class Todo {
-  constructor(todo) {
-    this.todos = todo;
-    this.checked = false;
-    this.createTodo()
-  }
-  createTodo() {
-    //create li element
-    const liElement = document.createElement("li");
-    const liText = document.createTextNode(this.todos);
-    liElement.appendChild(liText);
-    ul.appendChild(liElement);
-    arrayOfAllTodos.push(liText)
-    input.value = "";
-
-    //checkbox in li element
-    const checkbox = document.createElement("input");
-    checkbox.setAttribute("type", "checkbox");
-    liElement.appendChild(checkbox)
-
-
-    checkbox.addEventListener("click", function (e) {
-      if (e.target.checked) {
-        this.checked = this.checked !== false;
-      }
-      else {
-        this.checked = this.checked !== false;
-      }
-    })
-  }
+  //appendChild
+  newItem.appendChild(checkbox);
+  ul.appendChild(newItem);
 }
-
-//class in an function
-function newTodo() {
-  new Todo(input.value);
-}
-
-
-
 
 //all EventListener in an function
 function eventListener() {
   //press enter EventListener
   input.addEventListener("keyup", function (event) {
     if (event.key === "Enter") {
-      newTodo() 
+      createTodo();
     }
   });
 }
 
-eventListener()
+eventListener();
